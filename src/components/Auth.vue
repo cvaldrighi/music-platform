b431bf06078a14c3ed92bde5d1ed7b4090dfbe39
cvaldrighi <valdrighdev@gmail.com>
2022-07-08 14:02:46 -0300

    validation form configs

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -64,12 +64,13 @@
                         </button>
                     </form>
                     <!-- Registration Form -->
-                    <form v-show="tab === 'register'">
+                    <vee-form v-show="tab === 'register'" :validation-schema="schema">
                         <!-- Name -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Name</label>
-                            <input type="text" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded" placeholder="Enter Name" />
+                            <vee-field type="text" name="name" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                            duration-500 focus:outline-none focus:border-black rounded" placeholder="Enter Name" />
+                            <ErrorMessage class="text-red-600" name="name" />
                         </div>
                         <!-- Email -->
                         <div class="mb-3">
@@ -114,7 +115,7 @@
                     hover:bg-purple-700">
                             Submit
                         </button>
-                    </form>
+                    </vee-form>
                 </div>
             </div>
         </div>
@@ -127,6 +128,15 @@ export default {
     data() {
         return {
             tab: 'login',
+            schema: {
+                name: 'required|min:3|max:100|alpha_spaces',
+                email: '',
+                age: '',
+                password: '',
+                confirm_password: '',
+                country: '',
+                tos: '',
+            }
         }
     },
     computed: {
