f8fa916785098615ca19a26983c320016a916354
cvaldrighi <valdrighdev@gmail.com>
2022-07-12 10:53:54 -0300

    custom validating msgs

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -64,7 +64,12 @@
                         </button>
                     </form>
                     <!-- Registration Form -->
-                    <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
+                    <div class="text-white text-center font-bold p-5 mb-4" v-if="reg_show_alert"
+                        :class="reg_alert_variant">
+                        {{ reg_alert_msg }}
+                    </div>
+                    <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register"
+                        :initial-values="userData">
                         <!-- Name -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Name</label>
@@ -89,9 +94,12 @@
                         <!-- Password -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Password</label>
-                            <vee-field type="password" name="password" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                             duration-500 focus:outline-none focus:border-black rounded" placeholder="Password" />
-                            <ErrorMessage class="text-red-600" name="password" />
+                            <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                                <input type="password" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                                duration-500 focus:outline-none focus:border-black rounded" placeholder="Password"
+                                    v-bind="field" />
+                                <div class="text-red-600" v-for="error in errors" :key="error"> {{ error }}</div>
+                            </vee-field>
                         </div>
                         <!-- Confirm Password -->
                         <div class="mb-3">
@@ -120,7 +128,7 @@
                             <label class="inline-block">Accept terms of service</label>
                             <ErrorMessage class="text-red-600 block" name="tos" />
                         </div>
-                        <button type="submit" class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
+                        <button type="submit" :disabled="reg_in_submission" class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                         hover:bg-purple-700">
                             Submit
                         </button>
@@ -142,10 +150,17 @@ export default {
                 email: 'required|min:3|max:100|email',
                 age: 'required|min_value:18|max_value:100',
                 password: 'required|min:3|max:100',
-                confirm_password: 'confirmed:@password',
-                country: 'required|excluded:Antarctica',
-                tos: 'required',
-            }
+                confirm_password: 'passwords_mismatch:@password',
+                country: 'required|country_excluded:Antarctica',
+                tos: 'tos',
+            },
+            userData: {
+                country: 'USA',
+            },
+            reg_in_submission: false,
+            reg_show_alert: false,
+            reg_alert_variant: 'bg-blue-500',
+            reg_alert_msg: 'Please wait! Your account is being created.',
         }
     },
     computed: {
@@ -154,6 +169,13 @@ export default {
     methods: {
         ...mapMutations(['toggleAuthModal']),
         register(values) {
+            this.reg_show_alert = true;
+            this.reg_in_submission = true;
+            this.reg_alert_variant = 'bg-blue-500';
+            this.reg_alert_msg = 'Please wait! Your account it being created.';
+
+            this.reg_alert_variant = 'bg-green-500';
+            this.reg_alert_msg = 'Success! Your account has been created.';
             console.log(values);
         }
     }
