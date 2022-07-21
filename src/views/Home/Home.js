ec2306efd72e73aa7b046d7b446ed2ee1470aaaa
cvaldrighi <valdrighdev@gmail.com>
2022-07-21 15:47:28 -0300

    dynamic home page + songItem component

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,3 +1,23 @@
-export default {
+import { songsCollection } from "@/includes/firebase";
+import AppSongItem from '@/components/SongItem/SongItem.vue';
 
+export default {
+    name: 'Home',
+    components: {
+        AppSongItem
+    },
+    data() {
+        return {
+            songs: [],
+        }
+    },
+    async created() {
+        const snapshots = await songsCollection.get();
+        snapshots.forEach((document) => {
+            this.songs.push({
+                docID: document.id,
+                ...document.data(),
+            });
+        })
+    }
 }
